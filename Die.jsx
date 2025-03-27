e283e25b0d760612f02e2fc01d86ab7803887adc
ezekielswanson <75798690+ezekielswanson@users.noreply.github.com>
2025-03-26 19:55:13 -0500

    Updated code

--- Die.jsx
+++ Die.jsx
@@ -1,18 +1,26 @@
+
+//render child component for front end and pass properties from the component 
+//strucutre from the parent app
+
+
 import React from "react"
 export default function Dye(props) {
     const styles = {
-        backgroundColor: props.isHeld ? "#59E391" : "#fff"
+        backgroundColor:  props.isHeld ? "#59E391" : '#fff'
     }
     
     return (
+        
         <div 
             className="dye--element"
             onClick={props.onClick} 
-            style={styles}
+            styles={styles}
         >
             <span>{props.number}</span>
         </div>
+
     )
+
 }
 
 
