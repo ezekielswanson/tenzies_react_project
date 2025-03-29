4ed712632df9de6fdac8e8613a738035b9c6eff4
ezekielswanson <zeke@aptitude8.com>
2025-03-29 11:52:38 -0500

    Add gameWinner function

--- App.jsx
+++ App.jsx
@@ -19,14 +19,53 @@ export default function App() {
     -each dye 
     -dye number
     -are they all the same?
+    -is it frozen?
 
-
-
+    function that checks if all dye are equal
+    if they are game is won
 
 
 
     */
 
+
+    /*
+
+
+    What’s the purpose of the function?
+        -determine game winner by 
+          checkibg if all dye are equal
+    What’s the input?
+        -dye (the dye is held in the array so pass in the array)
+        -access it's number
+    What’s the output?
+        -all dye frozen
+        -pop in that says Game Winner!
+
+
+    Process? 
+    -pass in dye array
+    -loop through each dye element in the dye array, and access it's number value
+        -what type of for loop is needed?
+        -I need access to all each index (?) and it's number
+        -How do I check if all the dye's numbers are equal?
+
+    -Questions
+        - my assumption is since I already have a dye state 
+        stucture, and compponent created, I'm passing in dye as paramter
+
+    */
+
+
+    //Game winner function
+    const gameWinner = die.every(dye =>
+        //access the dye number and compare to the 
+        //dye number of the first dye object in the the array
+        dye.number === die[0].number && dye.isHeld
+
+        //return a value set by default
+
+    )
 
 
     //create structure of the object/element
@@ -96,12 +135,6 @@ Questions
 
 */
 
-
-
-function gameWon(die) {
-
-}
-  
 
 
 
