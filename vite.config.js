58a3664050ab0f42c245f6b3b399c0a5f327a78a
ezekielswanson <75798690+ezekielswanson@users.noreply.github.com>
2025-04-09 08:40:41 -0500

    Updates

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import {defineConfig} from "vite"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-	plugins: [
-		react()
-	],
-	base: '/tenzies_react_project/'
+	plugins: [react()],
+	// Remove any base path settings for Vercel
+	// Remove any outDir settings
 })
